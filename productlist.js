876c0c59f2a7acd7f547d6056e33bf775ded6027
philiptaiw <Philiptai95@gmail.com>
2023-08-31 12:12:40 +0200

    lavede productlist.js og nu sender siden videre

--- productlist.js
+++ productlist.js
@@ -1,5 +1,8 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category");
+
 // fetche
-fetch("https://kea-alt-del.dk/t7/api/products")
+fetch("https://kea-alt-del.dk/t7/api/products?category=" + category)
   .then((res) => res.json())
   .then(showProducts);
 
